1c942322f5507b7801ba9afecbf3bd0344da0411
Kazufumi Ohkawa <kerolinq@gmail.com>
2013-03-03 07:29:46 +0900

    テンプレート導入

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html", title="Flaski")
 
 if __name__ == "__main__":
     app.run()
