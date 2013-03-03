47c3730f437962e9a6426e12d9b26b5c83bc042f
Kazufumi Ohkawa <kerolinq@gmail.com>
2013-03-03 09:21:31 +0900

    モデルの設定

---
+++ flaski/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from flaski.database import Base
+from datetime import datetime
+
+
+class WikiContent(Base):
+    __tablename__ = 'wikicontents'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), unique=True)
+    body = Column(Text)
+    date = Column(DateTime, default=datetime.now())
+
+    def __init__(self, title=None, body=None, date=None):
+        self.title = title
+        self.body = body
+        self.date = date
+
+    def __repr__(self):
+        return '<Title %r>' % (self.title)
