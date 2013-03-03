9a7b0f5335ce7c516d4663b8301112bfd5072c6b
Kazufumi Ohkawa <kerolinq@gmail.com>
2013-03-03 12:08:59 +0900

    モデルを扱う

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from flaski.models import WikiContent
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
 @app.route("/")
 def hello():
-    return render_template("index.html", title="Flaski")
+    contents = WikiContent.query.all()
+    return render_template("index.html", contents=contents)
+
+
+@app.route("/<title>", methods=["GET"])
+def show_content(title):
+    content = WikiContent.query.filter_by(title=title).first()
+    if content is None:
+        abort(404)
+    return render_template("show_content.html", content=content)
 
 if __name__ == "__main__":
     app.run()
